894068b714e7c5898ed639d9c49754d551806106
Júlia Fick <132408071+JuFick@users.noreply.github.com>
2024-09-13 00:09:47 +0000

    inicio APP

---
+++ MeuPetIdeal/app/_layout.jsx
@@ -0,0 +1,18 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+
+  useFonts({
+    'outfit':require('./../assets/fonts/Lora-Regular.ttf'),
+    'outfit-medium':require('./../assets/fonts/Lora-Medium.ttf'),
+    'outfit-bold':require('./../assets/fonts/Lora-Bold.ttf'),
+
+  })
+
+  return (
+    <Stack>
+      <Stack.Screen name="index"/>
+    </Stack>
+  );
+}
---
+++ MeuPetIdeal/app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+
+            }}
+        >
+        <Text style={{fontFamily:'outfit',
+            fontSize:40
+        }}></Text>
+        </View>
+    );
+}
