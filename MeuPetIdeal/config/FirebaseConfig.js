22374d9164a2b423dad4e4b33c85bc6f1f9eb87c
Júlia Fick <julia.fick54@gmail.com>
2024-09-21 19:40:39 -0300

    criação do Slider no Firebase

--- MeuPetIdeal/config/FirebaseConfig.js
+++ MeuPetIdeal/config/FirebaseConfig.js
@@ -20,5 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app);
+// importa o nome do db, se for o default não é necessário
+export const db=getFirestore(app, 'default');
 // const analytics = getAnalytics(app);
