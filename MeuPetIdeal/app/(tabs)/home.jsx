7ff9f3fca827113eb196861fc93a86f99262f18c
Júlia Fick <julia.fick54@gmail.com>
2024-09-21 18:48:44 -0300

    arquivos de configuração Firebase

--- MeuPetIdeal/app/(tabs)/home.jsx
+++ MeuPetIdeal/app/(tabs)/home.jsx
@@ -1,10 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/Home/Header'
 
 export default function Home() {
   return (
-    <View>
-      <Text>home</Text>
+    <View style={{
+      padding:20, marginTop:20
+    }}>
+      {/* Header */}
+      <Header/>
+
+      {/* Slider */}
+      
+      
+      {/* Categorias */}
+
+      {/* Lista dos pets */}
+
+      {/* Adicionar novo Pet */}
     </View>
   )
 }
---
+++ MeuPetIdeal/config/FirebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore  } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    // chave de acesso na pasta .env que acessa por essa apiKey 
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "meupetideal-f97b4.firebaseapp.com",
+  projectId: "meupetideal-f97b4",
+  storageBucket: "meupetideal-f97b4.appspot.com",
+  messagingSenderId: "362569484322",
+  appId: "1:362569484322:web:e4f81b81d294d332b2227b",
+  measurementId: "G-YVRT2E96FN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
+// const analytics = getAnalytics(app);
